8d80e658fcb90d55ca5200a4b771112a903e3eb0
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-07-28 14:24:51 +0200

    start scrip

--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
+import { User } from '../models/User.model'
 
+type Overwrite<T1, T2> = { [Prop in Exclude<keyof T1, keyof T2>]: T1[Prop] } & T2 
 export interface ErrorProps {
 	message: string
 	place?: string
@@ -14,3 +16,5 @@ export type RouteProps = {
 	next: NextFunction
 	payload: Request & { payload?: jwt.JwtPayload }
 }
+
+export type NewUser = Overwrite<Pick<User, 'username' | 'password' | 'email'>, { password?: string, _id?: string }>
