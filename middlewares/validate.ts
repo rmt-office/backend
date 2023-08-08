7b9ba16e4c1ab1129fe4f3b00fabfe349b199091
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-08 17:05:14 +0200

    middleware to parse the requests

---
+++ middlewares/validate.ts
@@ -0,0 +1,20 @@
+import { AnyZodObject } from 'zod'
+import { RouteProps } from '../utils/types'
+
+export const validate =
+	(schema: AnyZodObject) =>
+	(req: RouteProps['payload'], res: RouteProps['res'], next: RouteProps['next']) => {
+		try {
+			schema.parse({
+				body: req.body,
+				params: req.params,
+				query: req.query,
+				payload: req.payload,
+			})
+			next()
+		} catch (error: any) {
+			error.status = 400
+			error.place = 'Validate Middleware'
+			next(error)
+		}
+	}
