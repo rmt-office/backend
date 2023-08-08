83f995bf78fc83f32d60a849f5a96d0dd8290d94
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-08 17:04:04 +0200

    (minor) changes in error handling routing

--- routes.ts
+++ routes.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express'
 import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes'
 import placesRoutes from './routes/places.routes'
-import { errorHandling } from "./routes/errorHandling";
-import { RouteProps } from "./utils/types";
+import { errorHandling } from './routes/errorHandling'
+import { RouteProps } from './utils/types'
 import pictureUpload from './routes/pictureUpload.routes'
 
 const router = Router()
@@ -14,8 +14,13 @@ router.use('/places', placesRoutes)
 
 router.use('/api', pictureUpload)
 
-router.use((error: RouteProps['error'], req: RouteProps['req'], res: RouteProps['res'], next:RouteProps['next'] ): void => {
-  errorHandling(error, res)
-})
+router.use(
+	(
+		error: RouteProps['error'],
+		req: RouteProps['req'],
+		res: RouteProps['res'],
+		next: RouteProps['next']
+	): RouteProps['res'] => errorHandling(error, res)
+)
 
 export default router
