aa017fb65c404331185f433f4e3eabe523a53926
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-08 17:04:58 +0200

    change error props to be able to receive zod errors

--- utils/types.ts
+++ utils/types.ts
@@ -1,14 +1,24 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
-export type Overwrite<T1, T2> = { [Prop in Exclude<keyof T1, keyof T2>]: T1[Prop] } & T2 
+export type Overwrite<T1, T2> = { [Prop in Exclude<keyof T1, keyof T2>]: T1[Prop] } & T2
 export interface ErrorProps {
 	message: string
 	place?: string
 	status: number
+	issues?: {
+		message: any
+		expected: string
+		received: string
+		path: string[]
+	}[]
 }
 
-interface PayloadOptions extends jwt.JwtPayload { email?: string, _id?: string, username?: string }
+interface PayloadOptions extends jwt.JwtPayload {
+	email?: string
+	_id?: string
+	username?: string
+}
 
 export type RouteProps = {
 	req: Request
@@ -18,4 +28,4 @@ export type RouteProps = {
 	payload: Request & { payload?: PayloadOptions }
 }
 
-export type Timestamps = { createdAt: NativeDate, updatedAt: NativeDate }
+export type Timestamps = { createdAt: NativeDate; updatedAt: NativeDate }
