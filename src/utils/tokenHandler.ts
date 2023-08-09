6d8e7ba024e2a588c31b58d8e1880a280ed084be
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-09 17:51:48 +0200

    add expiration

--- src/utils/tokenHandler.ts
+++ src/utils/tokenHandler.ts
@@ -1,13 +1,13 @@
-import  jwt  from "jsonwebtoken";
-import { NewUser } from "../models/User.model";
+import jwt from 'jsonwebtoken'
+import { NewUser } from '../models/User.model'
 
 export const createToken = (user: NewUser) => {
-  const payload = {
-    email: user.email,
-    _id: user._id,
-    username: user.username
-  }
-  const token = jwt.sign(payload, process.env.TOKEN_SECRET!)
+	const payload = {
+		email: user.email,
+		_id: user._id,
+		username: user.username,
+	}
+	const token = jwt.sign(payload, process.env.TOKEN_SECRET!, { expiresIn: '24h' })
 
-  return token
+	return token
 }
