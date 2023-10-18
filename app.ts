4d927d8639443f523a975be78cedef6361652550
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-10-18 11:49:32 +0200

    feature(cors config): setting cors to accept our frontend

--- app.ts
+++ app.ts
@@ -1,17 +1,24 @@
 import express from 'express'
 import 'dotenv/config'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import { connectDB } from './src/database/databaseConnection'
-
 import routes from './src/routes'
+
+const ORIGIN = process.env.ORIGIN
 
 const app = express()
 
 connectDB()
+
 app.use(express.json())
 app.use(morgan('dev'))
-
+app.use(
+	cors({
+		origin: ORIGIN,
+	})
+)
 app.use('/', routes)
 
 export default app
